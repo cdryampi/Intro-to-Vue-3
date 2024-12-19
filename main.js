a9d4df773b8a824db46045bb0949b552bba5f4ca
cdryampi <cdryampi@gmail.com>
2024-12-19 18:09:11 +0100

    code challenge #2

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
+class Product {
+  // class responsible for creating product objects
+  constructor(name, description, price, image) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.image = image;
+  }
+}
+
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      products: [
+        new Product(
+          "Blue Socks",
+          "A pair of warm, fuzzy blue socks",
+          12.99,
+          "assets/images/socks_blue.jpg"
+        ),
+        new Product(
+          "Green Socks",
+          "A pair of warm, fuzzy green socks",
+          14.99,
+          "assets/images/socks_green.jpg"
+        ),
+      ],
+    };
+  },
+});
