ad807eb217435d4b94b943b9216b43c7181aee72
cdryampi <cdryampi@gmail.com>
2024-12-19 17:08:58 +0100

    code challenge #1

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      title: "Vue3 Shopping Cart",
+      description: "This is a shopping cart",
+      products: {
+        "product-1": {
+          name: "socks blue",
+          price: 100,
+          qty: 0,
+          description: "This is a product 1",
+          image: "./assets/images/socks_blue.jpg",
+        },
+        "product-2": {
+          name: "socks green",
+          price: 200,
+          qty: 0,
+          description: "This is a product 2",
+          image: "./assets/images/socks_green.jpg",
+        },
+      },
+    };
+  },
+});
